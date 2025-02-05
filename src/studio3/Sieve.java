ed3a12ac0fe682f0040b79b75ce5e93e7b0fb319
Sideesh Reddy <Sideesh Reddy@Sideeshs-Laptop.dhcp.wustl.edu>
2025-02-05 15:49:46 -0600

    k

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,14 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Upto which number do you want us to use the Sieve of Erastosthenes for?");
+		Scanner in = new Scanner(System.in);
+
+	}
+
+}
