d77e96c941fb329271f566c12dc561d562423f94
Sideesh Reddy <Sideesh Reddy@Sideeshs-Laptop.dhcp.wustl.edu>
2025-02-05 15:49:56 -0600

    k

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -6,8 +6,19 @@ public class Sieve {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Upto which number do you want us to use the Sieve of Erastosthenes for?");
 		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Upto which number do you want us to use the Sieve of Erastosthenes for?");
+		int n = in.nextInt();
+		int[] numbers = new int[n];
+		for(int j = 0; j <= n; j++) {
+			break;
+		}
+		for (int i = 2; i < n; i++) {
+			for(int k = 0; k < n/2; k++) {
+			}
+			}
+		}
 
 	}
 
